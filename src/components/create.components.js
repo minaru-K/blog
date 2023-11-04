8ae144cf41ed7a75cec8b50bd0ad433f4ab94925
minaru <world.7d@gmail.com>
2023-11-04 18:56:18 +0300

    add action and validate for form

--- src/components/create.components.js
+++ src/components/create.components.js
@@ -1,8 +1,31 @@
 import { Component } from "../core/component";
+import { Form } from '../core/form';
+import { Validators } from "../core/validators";
 
 export class CreateComponents extends Component {
     constructor(id){
         super(id)
     }
-    
+
+    init() {
+        this.$el.addEventListener('submit', submitHandler.bind(this))
+        this.form = new Form(this.$el, {
+            title : [Validators.required],
+            fulltext: [Validators.required, Validators.minLength(10)]
+        })
+    }
+}
+
+function submitHandler(event){
+       event.preventDefault()
+
+    if (this.form.isValid()){
+        const formData = {
+            type : this.$el.type.value,
+            ...this.form.value()
+           }
+    this.form.clear()
+    }
+
+       
 }
